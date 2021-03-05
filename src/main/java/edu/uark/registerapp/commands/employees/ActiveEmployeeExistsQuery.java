bd77deed48301fc77b2bd080e155087893dd7cc4
uarkNathanSecrest <77640500+uarkNathanSecrest@users.noreply.github.com>
2021-03-04 21:40:57 -0600

    Update ActiveEmployeeExistsQuery.java

--- src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
+++ src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
@@ -1,4 +1,4 @@
-package edu.uark.registerapp.commands.employees;
+/*package edu.uark.registerapp.commands.employees;
 
 import org.apache.commons.lang3.StringUtils;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
@@ -17,4 +17,27 @@ public class ActiveEmployeeExistsQuery implements EmployeeRepository{
 
     @Autowire
     private EmployeeRepository employeeRepository;
+}*/
+
+package edu.uark.registerapp.commands.employees;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+@Service
+public class ActiveEmployeeExistsQuery implements VoidCommandInterface {
+	@Override
+	public void execute() {
+		if (!this.employeeRepository.existsByIsActive(true)) {
+			throw new NotFoundException("Employee");
+		}
+	}
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
 }
+
